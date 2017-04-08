9beda96ed513248266bf1a74557679ae09e24027
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-04-08 21:51:37 +0530

    Enable CORS

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import bundle from './bundle';
 import logger from './logger';
 import config from '../config';
@@ -9,6 +10,7 @@ import config from '../config';
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(cors());
 
 app.get('/bundle/*', bundle);
 
